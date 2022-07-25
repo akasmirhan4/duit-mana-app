3d5f63e26f1e82c8cde498a5fd34b726bf2b5eba
amirrulRecap <amirrul@itsrecap.com>
2022-07-25 21:48:08 +0800

    added fonts & added pulsed background for index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"radial": "radial-gradient(var(--tw-gradient-stops))",
+			},
+      fontFamily:{
+        'sans': ['Josefin Sans', 'sans-serif'],
+      }
+		},
+	},
+	plugins: [],
 };
