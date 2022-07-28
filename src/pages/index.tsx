d12fdb08ec243ce71f97563a3b870fe4f3e50161
amirrulRecap <amirrul@itsrecap.com>
2022-07-28 22:43:15 +0800

    added feather and button to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { trpc } from "../utils/trpc";
-
-type TechnologyCardProps = {
-	name: string;
-	description: string;
-	documentation: string;
-};
+import { Zap } from "react-feather";
 
 const Home: NextPage = () => {
-	const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
 	return (
 		<>
 			<Head>
@@ -22,11 +14,17 @@ const Home: NextPage = () => {
 
 			<main className="bg-radial from-[#320541] to-[#1B0536] h-screen">
 				<div className="container mx-auto flex flex-col items-center justify-center h-full p-4">
-					<div className="animate-pulse bg-radial from-[#fff2002c] to-[#ffffff00] w-full h-full flex justify-center items-center absolute">
-						<Image src="/images/question-mark.png" alt="Duit Mana?" width={200} height={200} />
+					<div className="animate-pulse bg-radial from-[#fff2002c] to-[#ffffff00] w-full h-full absolute pointer-events-none" />
+					<div className="flex-1 flex flex-col justify-center items-center">
+						<div className="animate-pulse">
+							<Image src="/images/question-mark.png" alt="Duit Mana?" width={200} height={200} />
+						</div>
+						<h1 className="text-2xl font-semibold text-white text-center pb-4 pt-2">Duit Mana?</h1>
+						<button className="border border-white text-white font-bold py-2 px-4 rounded inline-flex items-center hover:bg-white hover:text-[#320541] ease-in duration-100">
+							<Zap className="w-5 h-5 mr-2" />
+							<span>Start</span>
+						</button>
 					</div>
-					<div className="py-40" />
-					<h1 className="text-4xl font-bold text-white text-center">Duit Mana?</h1>
 					<h3 className="text-[#E6BBFF] text-center mt-1">Designed by akasmirhan4</h3>
 				</div>
 			</main>
