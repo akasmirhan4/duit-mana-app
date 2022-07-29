21c55c7309715483a25e0bd48add69eeac92622a
amirrulRecap <amirrul@itsrecap.com>
2022-07-29 12:09:24 +0800

    remove examples

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,9 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 
-export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", authRouter);
+export const appRouter = createRouter().transformer(superjson).merge("auth.", authRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
